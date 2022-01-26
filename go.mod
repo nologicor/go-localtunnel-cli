99755e686025384eb0b3e0176cb55e8250eeb48b
Nenad Balog <nenad@seller.tools>
2022-01-26 18:25:24 +0100

    Update to go mod and github actions

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nologicor/go-localtunnel-cli
+
+go 1.17
+
+require github.com/jonasfj/go-localtunnel v0.0.0-20170326223115-8a804488f275
--- main.go
+++ main.go
@@ -17,31 +17,50 @@ func main() {
 		os.Exit(1)
 	}
 
-	laddr := os.Args[1]
+	localAddress := os.Args[1]
 
-	lport, err := strconv.Atoi(os.Args[2])
+	fmt.Println(localAddress)
+
+	localPort, err := strconv.Atoi(os.Args[2])
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
-	rhost := os.Args[3]
+	remoteHost := os.Args[3]
 
 	subdomain := os.Args[4]
 
-	s := strings.Split(rhost, "://")
-	raddr := s[0] + "://" + subdomain + "." + s[1]
+	s := strings.Split(remoteHost, "://")
+	remoteAddrres := s[0] + "://" + subdomain + "." + s[1]
 
-	log.Printf("Local address %s:%d tunnel to %s", laddr, lport, raddr)
+	log.Printf("Local address %s:%d tunnel to %s", localAddress, localPort, remoteAddrres)
 
-	_, err = localtunnel.New(lport, laddr, localtunnel.Options{Subdomain: subdomain, BaseURL: rhost})
+	lt, err := localtunnel.New(
+		localPort,
+		localAddress,
+		localtunnel.Options{
+			Subdomain: subdomain,
+			BaseURL:   remoteHost,
+		},
+	)
 	if err != nil {
-		panic(err)
+		log.Fatalf("localtunnel initialization error: %s", err.Error())
 	}
-	listener, err := localtunnel.Listen(localtunnel.Options{})
+
+	defer lt.Close()
+
+	listener, err := localtunnel.Listen(localtunnel.Options{
+		Subdomain: subdomain,
+		BaseURL:   remoteHost,
+	})
 	if err != nil {
-		panic(err)
+		log.Fatalf("listener error: %s", err.Error())
 	}
+
 	log.Println("localtunnel is running...")
-	http.Serve(listener, nil)
+
+	if err := http.Serve(listener, nil); err != nil {
+		log.Fatalf("server error: %s", err.Error())
+	}
 }
